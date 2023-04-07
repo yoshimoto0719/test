8b78b4b35c85fc2f5ece14328eb4f7d69102cbbc
yoshimoto0719 <115342937+yoshimoto0719@users.noreply.github.com>
2023-04-07 06:05:11 +0000

    [make]if/else文

--- index.js
+++ index.js
@@ -10,10 +10,16 @@ const bigUnko = "Hello Hello World"
 let inoki =['いーち','にー','さーん','ダー!!'];
 
 //ループ文
-let index = 0;
-while(index < 5){
-  //繰り返したい命令  
-  index++; //index = 1
-}
+// let index = 0;
+// while(index < 5){
+//   //繰り返したい命令
+//   console.log(index);
+//   index++; //index = 1
+// }
 
-console.log(inoki[1]);
+//if / else
+if(inoki.length > 5){
+  console.log('ボンバイエ！');
+} else {
+  console.log('ボンバ...');
+}
