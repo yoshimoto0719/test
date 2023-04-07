52c81cf9e7ea691ab788a747f4cafa261a28904f
yoshimoto0719 <115342937+yoshimoto0719@users.noreply.github.com>
2023-04-07 05:51:55 +0000

    [make]ループ文

--- index.js
+++ index.js
@@ -6,4 +6,14 @@ let unko = "Hello Wolrd!";
 //定数
 const bigUnko = "Hello Hello World"
 
-console.log(bigUnko);
+//配列
+let inoki =['いーち','にー','さーん','ダー!!'];
+
+//ループ文
+let index = 0;
+while(index < 5){
+  //繰り返したい命令  
+  index++; //index = 1
+}
+
+console.log(inoki[1]);
