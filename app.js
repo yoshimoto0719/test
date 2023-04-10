d8eeb6caf904a491115f2d66d126da07ef06ce6b
yoshimoto0719 <115342937+yoshimoto0719@users.noreply.github.com>
2023-04-10 06:58:19 +0000

    [make]問題文①下準備完了

--- app.js
+++ app.js
@@ -11,4 +11,9 @@ const correct = 'ニンテンドーDS';
 
 console.log(document.getElementById('js-question'));
 
-document.getElementById('js-question').textContent = 'Question';
+document.getElementById('js-question').textContent = question;
+
+document.getElementsByTagName('button')[0].textContent = answers[0];
+document.getElementsByTagName('button')[1].textContent = answers[1];
+document.getElementsByTagName('button')[2].textContent = answers[2];
+document.getElementsByTagName('button')[3].textContent = answers[3];
