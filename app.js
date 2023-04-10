a12d3e2a8e297200974c4b47ccd5fd3dea5d1ad1
yoshimoto0719 <115342937+yoshimoto0719@users.noreply.github.com>
2023-04-10 06:49:26 +0000

    [make]問題文①作成

---
+++ app.js
@@ -0,0 +1,14 @@
+//問題文は１つのため、定数（const）を使用
+const question = 'ゲーム史上、最も売れたゲーム機は次のうちどれ？';
+//選択肢は複数あるため、配列を使用
+const answers = [
+    'スーパーファミコン',
+    'プレイステーション2',
+    'ニンテンドースイッチ',
+    'ニンテンドーDS'
+  ];
+const correct = 'ニンテンドーDS';
+
+console.log(document.getElementById('js-question'));
+
+document.getElementById('js-question').textContent = 'Question';
