6ad711b14e3abea87081b4394eafa83cdb91c139
yoshimoto0719 <115342937+yoshimoto0719@users.noreply.github.com>
2023-04-10 10:31:01 +0000

    [make]正誤判定機能

--- app.js
+++ app.js
@@ -11,9 +11,21 @@ const correct = 'ニンテンドーDS';
 
 console.log(document.getElementById('js-question'));
 
+//定数の文字列をHTMLに反映させる
 document.getElementById('js-question').textContent = question;
 
-document.getElementsByTagName('button')[0].textContent = answers[0];
-document.getElementsByTagName('button')[1].textContent = answers[1];
-document.getElementsByTagName('button')[2].textContent = answers[2];
-document.getElementsByTagName('button')[3].textContent = answers[3];
+const $button = document.getElementsByTagName('button');
+
+$button[0].textContent = answers[0];
+$button[1].textContent = answers[1];
+$button[2].textContent = answers[2];
+$button[3].textContent = answers[3];
+
+// ボタンをクリックしたら正誤判定
+$button[0].addEventListener('click', () => {
+    if(correct === $button[0].textContent){
+        window.alert('正解！');
+    } else {
+        window.alert('不正解！');
+    }
+});
