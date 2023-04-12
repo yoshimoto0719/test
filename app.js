51ecb150a11d6135ca4d2c9b7871bc5c30ec7c52
yoshimoto0719 <115342937+yoshimoto0719@users.noreply.github.com>
2023-04-12 10:34:18 +0000

    [リファクタリング6回目]clickHandler

--- app.js
+++ app.js
@@ -38,18 +38,11 @@ const clickHandler = (e) => {
 
 
 // ボタンをクリックしたら正誤判定
-$button[0].addEventListener('click', (e) => {
-    clickHandler(e);
-});
-
-$button[1].addEventListener('click', (e) => {
-    clickHandler(e);
-});
-
-$button[2].addEventListener('click', (e) => {
-    clickHandler(e);
-});
-
-$button[3].addEventListener('click', (e) => {
-    clickHandler(e);
-});
+let handlerIndex = 0;
+const buttonLength = $button.length;
+while (handlerIndex < buttonLength) {
+    $button[handlerIndex].addEventListener('click', (e) => {
+        clickHandler(e);
+    });
+    handlerIndex++;
+}
