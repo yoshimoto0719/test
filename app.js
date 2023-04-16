53803ea06831c97daa6511ee0567c5701f7da381
yoshimoto0719 <115342937+yoshimoto0719@users.noreply.github.com>
2023-04-16 08:43:27 +0000

    [delete]const quiz2

--- app.js
+++ app.js
@@ -8,9 +8,9 @@ const quiz = [
             ],
             correct: 'ニンテンドーDS';
     }, {
-        
+
     }, {
-        
+
     }
 ];
 
@@ -62,12 +62,3 @@ while (handlerIndex < buttonLength) {
     handlerIndex++;
 }
 
-const question = 'ゲーム史上、最も売れたゲーム機は次のうちどれ？';
-//選択肢は複数あるため、配列を使用
-const answers = [
-    'スーパーファミコン',
-    'プレイステーション2',
-    'ニンテンドースイッチ',
-    'ニンテンドーDS'
-  ];
-const correct = 'ニンテンドーDS';
